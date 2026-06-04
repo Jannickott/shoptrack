bff5d35c1ca081c2d8bb65ad5177d9dfa36d5372
Jannickott <jannickott@users.noreply.github.com>
2026-06-04 19:25:50 +0200

    ShopTrack — all features up to deburring timer

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from '../shoptrack_3.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
